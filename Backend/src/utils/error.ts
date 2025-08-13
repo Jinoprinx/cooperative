e2e9c895ad4a637ee1f6e17a26f52cdf1b81c800
jinoprinx <jino4rex@gmail.com>
2025-08-13 03:16:54 -0700

    Removed Backend folder

--- Backend/src/utils/error.ts
+++
@@ -1,7 +0,0 @@
-
-import { Response } from 'express';
-
-export const handleError = (res: Response, error: any, message: string) => {
-  console.error(message, error);
-  res.status(500).json({ message });
-};
