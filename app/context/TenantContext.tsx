3cd286c5420b4334625aa726045c3c4f175b1809
jinoprinx <jino4rex@gmail.com>
2026-04-25 22:40:00 -0700

    Added automated billing and platform dues collection with monnify

--- app/context/TenantContext.tsx
+++ app/context/TenantContext.tsx
@@ -14,6 +14,7 @@ interface TenantSettings {
     loanRules?: {
         maxApprovalAmount: number;
         interestRate: number;
+        deductInterestAtSource?: boolean;
     };
     registrationOpen: boolean;
 }
